04354b217f8c96f6e023d9789d0d76995e52cefc
Shozaburo Shimada <shozaburo.s@gmail.com>
2019-03-17 23:16:37 +0900

    update

---
+++ AoV_Simulator/Assets/Script/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+    public GameObject ArcanaUI;
+    public GameObject BuildUI;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void MenuButtonPressed(string menu) {
+        
+        switch (menu) {
+            case "arcana":
+                Debug.Log("Switch Arcana Menu");
+                ArcanaUI.SetActive(true);
+                BuildUI.SetActive(false);
+                break;
+            case "build":
+                ArcanaUI.SetActive(false);
+                BuildUI.SetActive(true);
+                Debug.Log("Switch Build Menu");
+                break;
+            default:
+                Debug.Log("Other");
+                break;
+        }
+    }
+
+}
